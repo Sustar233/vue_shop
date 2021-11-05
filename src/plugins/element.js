a0c026f043531c666b34dc6ca8b8462ae318b5a3
sustar <3sustar@gmail.com>
2021-11-05 18:25:18 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 弹框提示组件挂载到vue的原型对象
+Vue.prototype.$message = Message
