43d28128920b503b6b89ce1b47ceb91a0cf16b1f
sustar <3sustar@gmail.com>
2021-11-07 22:25:41 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -10,8 +10,15 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求的根路径axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http = axios
 
+// 关闭生产提示
 Vue.config.productionTip = false
 
 new Vue({
