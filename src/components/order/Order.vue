70a21829a50c216eb0590a97de42e8bb6c458b79
sustar <3sustar@gmail.com>
2021-11-18 14:48:26 +0800

    完成了报表功能的开发

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -74,7 +74,7 @@
           </el-form-item>
         </el-form>
         <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button @click="addressVisible = false">取 消</el-button>
           <el-button type="primary" @click="showProgressBox">确 定</el-button>
         </span>
       </el-dialog>
